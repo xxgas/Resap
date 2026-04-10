468cbe693058e2db239510eca762bac197a68f41
Agas Prasetya <agassetya0708@gmail.com>
2026-04-10 07:51:34 +0700

    first push

--- database/migrations/2026_03_01_000001_create_kategoris_table.php
+++
@@ -1,8 +0,0 @@
-<?php
-// redundant migration file - ignore
-use Illuminate\Database\Migrations\Migration;
-
-return new class extends Migration {
-    public function up(): void {}
-    public function down(): void {}
-};
